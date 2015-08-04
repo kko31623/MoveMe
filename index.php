ded59fed8d2c39b1d5a2c4f660f0c926ce7693eb
systemAce24185 <krishoza@Krishs-MacBook-Pro.local>
2015-08-04 17:07:23 +0100

    shtuff

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
                                                                                   
                                                                                                                      
 <?php 
-      ///*
+      
         $location = $_POST["location"];
         $ch = curl_init(); 
         curl_setopt($ch, CURLOPT_URL, "http://api.zoopla.co.uk/api/v1/property_listings.json?area=" . $location . "&api_key=c88442waawanw44ygzem2ee2"); 
@@ -24,6 +24,36 @@
           'description' => $item->description
     );
 }
+$i = 0;
+foreach ($listData as $result) {
+  $place_str  = $result['address'];
+  $place = explode(" ", $place_str);
+
+  //echo $place[1];  
+    $ch = curl_init(); 
+    curl_setopt($ch, CURLOPT_URL, 'https://maps.googleapis.com/maps/api/geocode/json?address=' . $place[0] . '+'. $place[1] .'+'. $place[3] .'+' . $place[4] . '&key=AIzaSyBJkKkDhn6Hily7xmGtPEVYe5ehoDWrF94'); 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+    $json = curl_exec($ch); 
+    curl_close($ch);      
+    $data = json_decode($json, true);
+   // if ($data['results'][0]['geometry']['location']['lat'] != "") {
+      # code...
+    
+    echo ($data['results'][0]['geometry']['location']['lat'].'<br>');
+    //echo $json['results'][0]['geometry']['location']['lng'];
+    //echo $json;
+ //  }else{
+    //  unset($listData[$i]);
+  // }
+  // $i = $i+1; 
+}
+
+
+    
+
+
+
+/*
 ?>
 <!DOCTYPE html>
 <html>
@@ -39,7 +69,7 @@
           <p>Property Type: <?= $result["property_type"]?></p>
           <p class="listing">Price:£<?= $result["price"] ?></p>
           <p>Address: <?=$result["address"] ?></p>
-        <!--   <p>Description: <?=$result["description"] ?></p> -->
+          <p>Description: <?=$result["description"] ?></p>
 
           
         </li>
